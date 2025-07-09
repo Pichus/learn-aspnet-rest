093e827fd4b65179f3817caea208de4674f1a345
pichus <dimatyzuk@gmail.com>
2025-07-09 20:23:13 +0300

    connect to a postgres db

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -1,15 +1,35 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using TodoApi.Models;
+using Microsoft.IdentityModel.Tokens;
 using TodoAPI.Middleware;
+using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
-// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(jwtOptions =>
+    {
+        jwtOptions.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"])),
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ValidAudience = config["JwtSettings:Audience"],
+            ValidIssuer = config["JwtSettings:Issuer"]
+        };
+    });
+
+builder.Services.AddAuthentication();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<TodoContext>(options => options.UseInMemoryDatabase("TodoList"));
+
+builder.Services.AddDbContext<TodoContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("TodoDatabase")));
 
 var app = builder.Build();
 
@@ -17,15 +37,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwaggerUi(options =>
-    {
-        options.DocumentPath = "/openapi/v1.json";
-    });
+    app.UseSwaggerUi(options => { options.DocumentPath = "/openapi/v1.json"; });
 }
 
 app.UseHttpsRedirection();
 
-app.UseMyLogging();
+// app.UseMyLogging();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
