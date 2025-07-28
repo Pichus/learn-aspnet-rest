c85b614c6f5cf769614e1f625730a23efb6a400c
pichus <dimatyzuk@gmail.com>
2025-07-28 18:55:34 +0300

    add test containers for postgres and redis

---
+++ TodoAPI.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.PostgreSql;
+using Testcontainers.Redis;
+
+namespace TodoAPI.IntegrationTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgresContainer;
+    private readonly RedisContainer _redisContainer;
+
+    public HttpClient HttpClient { get; set; } = null!;
+
+    public CustomWebApplicationFactory()
+    {
+        _postgresContainer = new PostgreSqlBuilder()
+            .WithImage("postgres:17.5-alpine3.22")
+            .WithPortBinding(8080, true)
+            .WithDatabase("test")
+            .WithUsername("admin")
+            .WithPassword("admin")
+            .Build();
+        
+        _redisContainer = new RedisBuilder()
+            .WithImage("redis:8.0.3-alpine")
+            .WithPortBinding(6379, true)
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _postgresContainer.StartAsync();
+        await _redisContainer.StartAsync();
+
+        HttpClient = CreateClient();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _postgresContainer.DisposeAsync();
+        await _redisContainer.DisposeAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        Environment.SetEnvironmentVariable("ConnectionStrings:TodoDatabase", _postgresContainer.GetConnectionString());
+    }
+}
